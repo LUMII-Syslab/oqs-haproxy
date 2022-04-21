21e5095f4bde8bcbe39da21061840ecbe7f560bf
Sergejs Kozlovičs <sergejs.kozlovics@gmail.com>
2022-04-22 00:16:45 +0300

    added oqs-openssl build scripts for cygwin

---
+++ build_haproxy_win64.bat
@@ -0,0 +1,28 @@
+:: Downloads and installs cygwin64 for windows and
+:: invokes child scripts to build haproxy with open-quantum-safe support.
+::
+:: Copyright (c) Institute of Mathematics and Computer Science, University of Latvia
+:: Licence: MIT
+:: Contributors:
+::   Sergejs Kozlovics, 2022
+
+set CYGWIN_INSTALLER_PATH=cygwin_installer
+set CYGWIN_SITE=https://ftp.fsn.hu/pub/cygwin/
+
+mkdir %CYGWIN_INSTALLER_PATH%
+pushd %CYGWIN_INSTALLER_PATH%
+
+:: download cygwin installer
+if not exist setup-x86_64.exe curl.exe -o setup-x86_64.exe https://www.cygwin.com/setup-x86_64.exe
+
+:: install default cygwin packages
+if not exist C:\cygwin64\bin setup-x86_64.exe -q --wait --site %CYGWIN_SITE%
+
+:: install additional cygwin packages
+setup-x86_64.exe -q --wait -P git,perl,cmake,make,gcc-core,ninja,libtool
+
+popd
+
+pushd child_scripts
+C:\cygwin64\bin\bash --login -c "./build_oqs-openssl_cygwin64.sh"
+popd
---
+++ child_scripts/build_oqs-openssl_cygwin64.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Copyright (c) Institute of Mathematics and Computer Science, University of Latvia
+# Licence: MIT
+# Contributors:
+#   Sergejs Kozlovics, 2022
+
+export SCRIPT_PATH=`pwd`
+
+# liboqs build type variant; maximum portability of image:
+export LIBOQS_BUILD_DEFINES="-DOQS_DIST_BUILD=ON"
+
+# installation path
+export INSTALL_PATH=/oqs/openssl
+# variant: /usr/local
+
+if [ -f "$INSTALL_PATH/bin/openssl" ]; then
+    echo oqs-openssl has already been installed; skipping the build process
+    exit
+fi
+
+git clone --branch OQS-OpenSSL_1_1_1-stable https://github.com/open-quantum-safe/openssl.git
+git clone --branch main https://github.com/open-quantum-safe/liboqs.git
+
+cd $SCRIPT_PATH/liboqs
+mkdir build && cd build && if [[ -z "$MAKE_DEFINES" ]] ; then nproc=$(getconf _NPROCESSORS_ONLN) && MAKE_DEFINES="-j $nproc"; fi && cmake .. ${LIBOQS_BUILD_DEFINES} -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${SCRIPT_PATH}/openssl/oqs && make $MAKE_DEFINES && make install
+
+cd $SCRIPT_PATH/openssl
+LDFLAGS="-Wl,-rpath -Wl,$INSTALL_PATH/lib" ./Configure Cygwin-x86_64 -lm --prefix=$INSTALL_PATH && if [[ -z "$MAKE_DEFINES" ]] ; then nproc=$(getconf _NPROCESSORS_ONLN) && MAKE_DEFINES="-j $nproc"; fi && make $MAKE_DEFINES && make install
+## ^^^was: make install_sw
+
+cp $SCRIPT_PATH/liboqs/build/bin/cygoqs-0.dll $INSTALL_PATH/bin/
+cp $SCRIPT_PATH/openssl/cygcrypto-1.1.dll $INSTALL_PATH/bin/
+cp $SCRIPT_PATH/openssl/cygssl-1.1.dll $INSTALL_PATH/bin/
+cp /cygdrive/c/cygwin64/bin/cygwin1.dll $INSTALL_PATH/bin/
+cp /cygdrive/c/cygwin64/bin/cyggcc_s-seh-1.dll $INSTALL_PATH/bin/
