1a0851b1c1c2022f3d38fc15a8776dd29ff04669
Sergejs Kozlovičs <sergejs.kozlovics@gmail.com>
2022-04-29 01:00:19 +0300

    fixed typo

--- build_haproxy_win64.bat
+++ build_haproxy_win64.bat
@@ -34,4 +34,4 @@ popd
 :: invoking child scripts (cygwin understands cd in the Windows path syntax; cd is required)
 mkdir %BUILD_PATH%
 C:\cygwin64\bin\bash --login -c "cd '%~dp0' && dos2unix ./_build_openssl_cygwin64.sh && ./_build_openssl_cygwin64.sh %BUILD_PATH% %INSTALL_PATH%"
-C:\cygwin64\bin\bash --login -c "cd '%~dp0' && dos2unix ./_build_haproxy_common.sh ./_build_haproxy_cygwin64.sh && ./build_haproxy_cygwin64.sh %BUILD_PATH% %INSTALL_PATH% %INSTALL_PATH% TARGET=cygwin"
+C:\cygwin64\bin\bash --login -c "cd '%~dp0' && dos2unix ./_build_haproxy_common.sh ./_build_haproxy_cygwin64.sh && ./_build_haproxy_cygwin64.sh %BUILD_PATH% %INSTALL_PATH% %INSTALL_PATH% TARGET=cygwin"
